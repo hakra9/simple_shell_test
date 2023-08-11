a71d5d3149e03bfce9b9c5047f49a3f8ad8e4a29
hama <hama@example.com>
2023-08-11 15:16:31 -0700

    j

--- simple_shell.c
+++ simple_shell.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
- 
+#include <stdio.h>
+#include <stdlib.h> 
 int main ()
 {
-	char *buffer = NULL;
-	size_t buffer_size = 0;
+	char *buf = NULL;
+	size_t buf_size = 0;
 	char *token ;
 	char **array ;
-	int status ,i=0 ,n_char;
+	int status ,j=0 ,n_char;
 	pid_t pid;
 
 	while(1)
 	{ write(1, "$", 2);
-		n_char = getline (&buffer , &buffer_size ,stdin);
+		n_char = getline (&buf , &buf_size ,stdin);
 		array = malloc(sizeof (char * ) * 1024);
-		token = strtok(buffer ," \t\n");
+		token = strtok(buf ," \t\n");
 		while(token)
 		{
 			array[i] = token ;
 			token = strtok (NULL , " \t\n");
-			i++;
+			j++;
 		}
-		array [i] = NULL;
+		array [j] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{if (execve(array[0] , array , NULL) == -1)
@@ -32,9 +31,7 @@ int main ()
 		}
 		else 
 			wait(&status);
-		i=0;
+		j=0;
 		free (array);
 	}
 }
-
-
