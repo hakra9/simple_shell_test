89e8c7503c2eef08beb9eea77a0f76032f5c7df9
hama <hama@example.com>
2023-08-16 13:06:59 -0700

    h

---
+++ hamacode/main.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv, char **env)
+{
+    char *buf = NULL;
+    size_t buf_size = 0;
+    char *token;
+    char **array;
+    int n_char;
+    pid_t pid;
+    char *prog_name ;
+
+    prog_name = argv[0] ;
+     while (1)
+    {
+        display_prompt();
+        n_char = getline(&buf, &buf_size, stdin);
+        if (n_char == -1) 
+        {
+            printf("\nExiting shell.\n");
+            break;
+        }
+        if (buf[strlen(buf) - 1] == '\n')
+            buf[strlen(buf) - 1] = '\0';
+        array = tokenize(buf, buf_size);
+
+        if (strcmp(array[0], "exit") == 0) {
+            exit_shell(); // Call the exit built-in function
+            break; // Exit the loop
+        }
+
+        forking(array, pid, prog_name );
+        free(array);
+    }
+    free(buf);
+    return (0);
+}
