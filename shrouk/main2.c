4fc46da53c3993825f813e0cbc9a026b3547c08c
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-22 00:46:03 +0200

    m

--- shrouk/main2.c
+++ shrouk/main2.c
@@ -17,7 +17,8 @@ int main(int argc, char **argv, char **env)
     prog_name = argv[0];
     while (1)
     {
-        display_prompt();
+        if mode(1);
+            display_prompt();
         n_char = getline(&buf, &buf_size, stdin);
         if (n_char == -1) 
         {
