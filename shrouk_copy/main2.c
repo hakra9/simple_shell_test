5b05c137ff846626b4b3fb3250c9a119ec040766
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-22 20:05:51 +0200

    m

--- shrouk_copy/main2.c
+++ shrouk_copy/main2.c
@@ -17,17 +17,12 @@ int main(int argc, char **argv, char **env)
     char msg_pgn[300];
 
     prog_name = argv[0];
-    //if (argc > 1)
-        //mode = 0;
-    //else{
-        //mode = 1;
-   // }   
     mode = (argc > 1 ? 0 : 1);
-    if (mode == 1){
-            display_prompt();
-        }
     while (1)
     {
+        if (mode == 1){
+            display_prompt();
+        }
         n_char = getline(&buf, &buf_size, stdin);
         if (n_char == -1) 
         {
@@ -41,16 +36,13 @@ int main(int argc, char **argv, char **env)
             if (built_ins(array, env) == 1)
                 temp = find_path(env, array[0]);
             if (temp == NULL){
-                printf("%s: 1: %s: not found\n", prog_name, array[0]);
+                perror(printf("%s: 1: %s: not found\n", prog_name, array[0]));
                 continue;
             }
             array[0] = find_path(env, array[0]);
         }
         forking(array, prog_name, env);
         free(array);
-        if (mode == 1){
-            display_prompt();
-        }
     }
     free(buf);
     return (0);
