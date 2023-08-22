9baa903456f9d2b9894f76468858bc1bf72e0712
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-22 20:06:38 +0200

    m

--- shrouk_copy/main2.c
+++ shrouk_copy/main2.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv, char **env)
             if (built_ins(array, env) == 1)
                 temp = find_path(env, array[0]);
             if (temp == NULL){
-                perror(printf("%s: 1: %s: not found\n", prog_name, array[0]));
+                printf("%s: 1: %s: not found\n", prog_name, array[0]);
                 continue;
             }
             array[0] = find_path(env, array[0]);
