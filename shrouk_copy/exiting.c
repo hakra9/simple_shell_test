f739ff4149446f439e6381acc8c4c16484280771
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-23 00:10:13 +0200

    task gcc

--- shrouk_copy/exiting.c
+++ shrouk_copy/exiting.c
@@ -9,9 +9,9 @@
 /**
  * exit_shell - for exite the shell
  * returne : Exit the program with status code 0 (success)
- */
+*/
 void exit_shell() 
 {
-    exit(0); // Exit the program with status code 0 (success)
+    exit(0); 
 }
 
