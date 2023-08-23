ffbfc758f4e60c2876199eff6bab51f16a1a9f0c
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-23 08:38:59 +0200

    err correction

--- shrouk_copy/main2.c
+++ shrouk_copy/main2.c
@@ -46,7 +46,7 @@ int main(int argc, char **argv, char **env)
                 temp = find_path(env, array[0]);
             if (temp == NULL && mode == 0){
                 fprintf(stderr,"%s: 1: %s: not found\n", prog_name, array[0]);
-                break;
+                exit(127);
             }
             if (temp != NULL)
                 array[0] = find_path(env, array[0]);
