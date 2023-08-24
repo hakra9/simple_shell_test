d7242ee3822abd5b976355a5d629bc781c4542cc
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-24 23:10:26 +0200

    fixing env

--- shrouk_copy/shrouk_check/built_ins.c
+++ shrouk_copy/shrouk_check/built_ins.c
@@ -11,10 +11,12 @@ int built_ins(char **array, char **env)
 	if (strcmp(array[0], "exit") == 0)
 	{
 		exit_shell();
+		return (1);
 	}
 	if (strcmp(array[0], "env") == 0)
 	{
 		print_environment(env);
+		return (1);
 	}
-	return (1);
+	return (0);
 }
